41fca7c8bbf379692511797e068c96c2aff0098b
saqiba123 <saqibajuna@gmail.com>
2026-03-31 14:20:04 +0500

    Checkpoint 1 - cg66ed0a67

---
+++ .init/.linter.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd /tmp/code-generation/responsive-event-explorer-245219-245233/events_frontend
+npm run lint
+ESLINT_EXIT_CODE=$?
+npm run build
+BUILD_EXIT_CODE=$?
+if [ $ESLINT_EXIT_CODE -ne 0 ] || [ $BUILD_EXIT_CODE -ne 0 ]; then
+   exit 1
+fi
+
